674c514b73defe0a56a8d0cdafb7675f5d828a71
wrw <754531453@qq.com>
2021-03-01 00:42:33 +0800

    20210228

---
+++ Daily/src/v202102/Easy_20200228.java
@@ -0,0 +1,24 @@
+package v202102;
+
+/**
+ * @author wrw
+ * @date 2021/2/28 22:52
+ * 如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+ * <p>
+ * 当给定的数组 A是单调数组时返回 true，否则返回 false。
+ */
+public class Easy_20200228 {
+    public static boolean isMonotonic(int[] A) {
+        boolean inc = true, dec = true;
+        int n = A.length;
+        for (int i = 0; i < n - 1; ++i) {
+            if (A[i] > A[i + 1]) {
+                inc = false;
+            }
+            if (A[i] < A[i + 1]) {
+                dec = false;
+            }
+        }
+        return inc || dec;
+    }
+}
